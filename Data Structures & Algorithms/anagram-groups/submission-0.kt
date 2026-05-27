eba8dfcee1e98dda9ec20c45fe094778bfb78680
Yuxuan Zhao <yuxuanz.work@outlook.com>
2026-05-27 19:36:55 -0300

    Add: anagram-groups - submission-0

---
+++ Data Structures & Algorithms/anagram-groups/submission-0.kt
@@ -0,0 +1,21 @@
+class Solution {
+    fun groupAnagrams(strs: Array<String>): List<List<String>> {
+
+        val map = HashMap<String, MutableList<String>>()
+
+        for (s in strs) {
+
+            val count = IntArray(26)
+
+            for (c in s) {
+                count[c - 'a']++
+            }
+
+            val key = count.joinToString("#")
+
+            map.getOrPut(key) { mutableListOf() }.add(s)
+        }
+
+        return map.values.toList()
+    }
+}
