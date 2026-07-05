c6b414c5c6c1b6d0835668a0e9450fc527a8039f
Yuxuan Zhao <yuxuanz.work@outlook.com>
2026-07-05 19:36:05 -0300

    Add: nested-list-weight-sum-ii - submission-0

---
+++ Data Structures & Algorithms/nested-list-weight-sum-ii/submission-0.py
@@ -0,0 +1,82 @@
+# """
+# This is the interface that allows for creating nested lists.
+# You should not implement it, or speculate about its implementation
+# """
+#class NestedInteger:
+#    def __init__(self, value=None):
+#        """
+#        If value is not specified, initializes an empty list.
+#        Otherwise initializes a single integer equal to value.
+#        """
+#
+#    def isInteger(self):
+#        """
+#        @return True if this NestedInteger holds a single integer, rather than a nested list.
+#        :rtype bool
+#        """
+#
+#    def add(self, elem):
+#        """
+#        Set this NestedInteger to hold a nested list and adds a nested integer elem to it.
+#        :rtype void
+#        """
+#
+#    def setInteger(self, value):
+#        """
+#        Set this NestedInteger to hold a single integer equal to value.
+#        :rtype void
+#        """
+#
+#    def getInteger(self):
+#        """
+#        @return the single integer that this NestedInteger holds, if it holds a single integer
+#        Return None if this NestedInteger holds a nested list
+#        :rtype int
+#        """
+#
+#    def getList(self):
+#        """
+#        @return the nested list that this NestedInteger holds, if it holds a nested list
+#        Return None if this NestedInteger holds a single integer
+#        :rtype List[NestedInteger]
+#        """
+
+class Solution:
+    def depthSumInverse(self, nestedList: List[NestedInteger]) -> int:
+
+        maxDepth = 0
+        memo = []
+
+        def dfs(nestedInteger, depth):
+            nonlocal maxDepth
+            if nestedInteger.isInteger():
+                maxDepth = max(maxDepth, depth)
+                memo.append((nestedInteger.getInteger(), depth))
+                print(memo)
+            else:
+                for item in nestedInteger.getList():
+                    dfs(item, depth + 1)
+        
+        for item in nestedList:
+            dfs(item, 1)
+
+        result = 0
+        for value, depth in memo:
+            result += (maxDepth - depth + 1) * value
+        return result
+
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
