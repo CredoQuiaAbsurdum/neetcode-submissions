16cd2adcffe84a7438b1f2b5cab63f7819053aab
Yuxuan Zhao <yuxuanz.work@outlook.com>
2026-07-26 22:44:16 -0300

    Add: coin-change - submission-2

---
+++ Data Structures & Algorithms/coin-change/submission-2.py
@@ -0,0 +1,23 @@
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+
+        coins.sort(reverse=True)
+
+        memo = {}
+        def helper(remain):
+            if remain == 0:
+                return 0
+            if remain < 0:
+                return -1
+            if remain in memo:
+                return memo[remain]
+            
+            least = float('inf')
+            for coin in coins:
+                current = helper(remain - coin)
+                if current >= 0:
+                    least = min(least, current + 1)
+            memo[remain] = -1 if least == float('inf') else least
+            return memo[remain]
+        
+        return helper(amount)
